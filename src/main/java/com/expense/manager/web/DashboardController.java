4770a1974e5ce57198abbeed08c08816fbfb1688
MichalPietrus <grahul2121@gmail.com>
2020-09-22 11:49:51 +0200

    Final version, fixed dashboard
    Fixed Dashboard
    - Added progress bar and javascript to it

--- src/main/java/com/expense/manager/web/DashboardController.java
+++ src/main/java/com/expense/manager/web/DashboardController.java
@@ -1,6 +1,5 @@
 package com.expense.manager.web;
 
-import com.expense.manager.MapUtil;
 import com.expense.manager.Pojo.CategoryRanking;
 import com.expense.manager.model.Transaction;
 import com.expense.manager.service.TransactionService;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -26,24 +26,67 @@ public class DashboardController {
 
     @GetMapping
     public String showDashboard(Model model, Principal principal) {
+
+
+        // Finds All transactions done by the user in last month.
+
+
         String username = principal.getName();
         LocalDate localDateNow = LocalDate.now();
         LocalDate localDateBefore = localDateNow.minusMonths(1);
-        List<Transaction> transactions = transactionService.findAllTransactionsByUsernameBetweenTwoDates(username, localDateBefore, localDateNow)
-                .stream().sorted(Comparator.comparing(Transaction::getAmount).reversed()).collect(Collectors.toList());
-        Map<String,Long> categoriesRankingIncomeMap = transactions.stream()
-                .filter(transaction -> transaction.getType().equals("income")).collect(Collectors.toMap(Transaction::getCategory,Transaction::getAmount, Long::sum));
-        MapUtil.sortByValue(categoriesRankingIncomeMap);
-        Map<String,Long> categoriesRankingOutcomeMap = transactions.stream()
-                .filter(transaction -> transaction.getType().equals("outcome")).collect(Collectors.toMap(Transaction::getCategory,Transaction::getAmount, Long::sum));
-        MapUtil.sortByValue(categoriesRankingOutcomeMap);
+        List<Transaction> transactions = transactionService.findAllTransactionsByUsernameBetweenTwoDates(username, localDateBefore, localDateNow);
+
+
+        // Calculating last month balance / income / outcome
+
+
+        long lastMonthBalance = transactions
+                .stream().mapToLong(Transaction::getAmount).sum();
+        long lastMonthIncome = transactions.stream()
+                .filter(transaction -> transaction.getType().equals("income")).mapToLong(Transaction::getAmount).sum();
+        long lastMonthOutcome = transactions.stream()
+                .filter(transaction -> transaction.getType().equals("outcome")).mapToLong(Transaction::getAmount).sum();
+
+
+        // Calculating which three categories were the most profitable in last month.
+
+
         List<CategoryRanking> categoriesIncomeRanking = new ArrayList<>();
+        Map<String,Long> categoriesRankingIncomeMapSorted = new LinkedHashMap<>();
+
+        Map<String,Long> categoriesRankingIncomeMap = transactions.stream()
+                .filter(transaction -> transaction.getType().equals("income"))
+                .collect(Collectors.toMap(Transaction::getCategory,Transaction::getAmount, Long::sum));
+
+        categoriesRankingIncomeMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> categoriesRankingIncomeMapSorted.put(x.getKey(), x.getValue()));
+
+        getThreeCategoriesWithBiggestOrLowestValue(categoriesRankingIncomeMapSorted, categoriesIncomeRanking,lastMonthIncome,"income");
+
+
+        // Calculating which three categories were the least profitable in last month.
+
+
+        Map<String,Long> categoriesRankingOutcomeMapSorted = new LinkedHashMap<>();
         List<CategoryRanking> categoriesOutcomeRanking = new ArrayList<>();
-        getThreeCategoriesWithBiggestOrLowestValue(categoriesRankingIncomeMap, categoriesIncomeRanking);
-        getThreeCategoriesWithBiggestOrLowestValue(categoriesRankingOutcomeMap, categoriesOutcomeRanking);
-        long lastMonthBalance = transactions.stream().mapToLong(Transaction::getAmount).sum();
-        long lastMonthIncome = transactions.stream().filter(transaction -> transaction.getType().equals("income")).mapToLong(Transaction::getAmount).sum();
-        long lastMonthOutcome = transactions.stream().filter(transaction -> transaction.getType().equals("outcome")).mapToLong(Transaction::getAmount).sum();
+
+        Map<String,Long> categoriesRankingOutcomeMap = transactions.stream()
+                .filter(transaction -> transaction.getType().equals("outcome"))
+                .collect(Collectors.toMap(Transaction::getCategory,Transaction::getAmount, Long::sum));
+
+        categoriesRankingOutcomeMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(x -> categoriesRankingOutcomeMapSorted.put(x.getKey(), x.getValue()));
+
+        getThreeCategoriesWithBiggestOrLowestValue(categoriesRankingOutcomeMapSorted, categoriesOutcomeRanking,lastMonthOutcome,"outcome");
+
+
+        // Model attributes
+
+
         model.addAttribute("categoriesIncomeRanking",categoriesIncomeRanking);
         model.addAttribute("categoriesOutcomeRanking",categoriesOutcomeRanking);
         model.addAttribute("transactions",transactions);
@@ -53,12 +96,20 @@ public class DashboardController {
         return "Dashboard";
     }
 
-    private void getThreeCategoriesWithBiggestOrLowestValue(Map<String, Long> categoriesRankingIncomeMap, List<CategoryRanking> categoriesIncomeRanking) {
+    public static void getThreeCategoriesWithBiggestOrLowestValue(Map<String, Long> categoriesRankingIncomeMap, List<CategoryRanking> categoriesIncomeRanking, long lastMonthIncomeOrOutcome, String type) {
         for(Map.Entry<String,Long> ranking : categoriesRankingIncomeMap.entrySet()) {
             if(categoriesIncomeRanking.size() < 3) {
                 CategoryRanking categoryRanking = new CategoryRanking();
                 categoryRanking.setName(ranking.getKey());
                 categoryRanking.setAmount(ranking.getValue());
+                double percent;
+                if(type.equals("outcome"))
+                    percent = (-(double)categoryRanking.getAmount() / -(double)lastMonthIncomeOrOutcome) * 100f;
+                else
+                    percent = ((double)categoryRanking.getAmount() / (double)lastMonthIncomeOrOutcome) * 100f;
+                DecimalFormat decimalFormat = new DecimalFormat("###.##");
+                decimalFormat.format(percent);
+                categoryRanking.setPercentageContribution(percent);
                 categoriesIncomeRanking.add(categoryRanking);
             }
         }
---
+++ src/main/resources/static/js/changeCategories.js
@@ -0,0 +1,167 @@
+function changeIncomeCategories() {
+    let IncomeFromDate = document.getElementById("IncomeFromDate").value;
+    let IncomeToDate = document.getElementById("IncomeToDate").value;
+    if(IncomeFromDate && IncomeToDate) {
+        let incomeData = {
+            fromDate: IncomeFromDate,
+            toDate: IncomeToDate,
+            type: "income"
+        };
+        $.ajax({
+            url: "/categories-filter/by-date",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(incomeData)
+        }).done(function (data) {
+            let incomesColumn = document.getElementById("incomesColumn");
+            incomesColumn.innerHTML = ' ';
+            let categoriesIncomeRanking = data;
+            let firstDiv;
+            let secondDiv;
+            let thirdDiv;
+            if(categoriesIncomeRanking.length >= 1) {
+                firstDiv = ` 
+                <div>
+                    <br>
+                    <i class="transaction-category-name-progress-bar">${categoriesIncomeRanking[0].name}</i>
+                    <div class="progress progress-bar-dimensions">
+                        <div id="firstDivProgressBar" class="progress-bar progress-bar-striped bg-success progress-bar-animated" 
+                        role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        ${Math.round(categoriesIncomeRanking[0].percentageContribution * 10)/10 + '%'}</div>
+                    </div>
+                    <br>
+                    <i class="transaction-category-amount-progress-bar">${categoriesIncomeRanking[0].amount + ' zł'}</i>
+                </div>
+                `;
+                incomesColumn.innerHTML = firstDiv;
+                let firstProgressBar = document.getElementById("firstDivProgressBar");
+                firstProgressBar.style.width = `${categoriesIncomeRanking[0].percentageContribution}%`;
+            }
+            if(categoriesIncomeRanking.length >= 2) {
+                secondDiv = `
+                <div>
+                    <br>
+                    <hr>
+                    <i class="transaction-category-name-progress-bar">${categoriesIncomeRanking[1].name}</i>
+                    <div class="progress progress-bar-dimensions">
+                        <div id="secondDivProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" 
+                        role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        ${Math.round(categoriesIncomeRanking[1].percentageContribution * 10)/10 + '%'}</div>
+                    </div>
+                    <br>
+                    <i class="transaction-category-amount-progress-bar">${categoriesIncomeRanking[1].amount + ' zł'}</i>
+                </div>
+                `;
+                incomesColumn.innerHTML += secondDiv;
+                let secondProgressBar = document.getElementById("secondDivProgressBar");
+                secondProgressBar.style.width = `${categoriesIncomeRanking[1].percentageContribution}%`;
+            }
+            if(categoriesIncomeRanking.length === 3) {
+                thirdDiv = `
+                <div>
+                    <br>
+                    <hr>
+                    <i class="transaction-category-name-progress-bar">${categoriesIncomeRanking[2].name}</i>
+                    <div class="progress progress-bar-dimensions">
+                        <div id="thirdDivProgressBar" class="progress-bar progress-bar-striped bg-danger progress-bar-animated"
+                             role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                             ${Math.round(categoriesIncomeRanking[2].percentageContribution * 10)/10 + '%'}</div>
+                   </div>
+                   <br>
+                    <i class="transaction-category-amount-progress-bar">${categoriesIncomeRanking[2].amount + ' zł'}</i>
+                </div>
+            `;
+                incomesColumn.innerHTML += thirdDiv;
+                let thirdProgressBar = document.getElementById("thirdDivProgressBar");
+                thirdProgressBar.style.width = `${categoriesIncomeRanking[2].percentageContribution}%`;
+            }
+            console.log(data)
+        }).fail(function (data) {
+            console.log(data);
+        });
+    }
+}
+
+function changeOutcomeCategories() {
+    let OutcomeFromDate = document.getElementById("OutcomeFromDate").value;
+    let OutcomeToDate = document.getElementById("OutcomeToDate").value;
+    if(OutcomeFromDate && OutcomeToDate) {
+        let outcomeData = {
+            fromDate: OutcomeFromDate,
+            toDate: OutcomeToDate,
+            type: "outcome"
+        };
+        $.ajax({
+            url: "/categories-filter/by-date",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(outcomeData)
+        }).done(function (data) {
+            let outcomesColumn = document.getElementById("outcomesColumn");
+            outcomesColumn.innerHTML = ' ';
+            let categoriesOutcomeRanking = data;
+            let firstDiv;
+            let secondDiv;
+            let thirdDiv;
+            if(categoriesOutcomeRanking.length >= 1) {
+                firstDiv = ` 
+                <div>
+                    <br>
+                    <i class="transaction-category-name-progress-bar">${categoriesOutcomeRanking[0].name}</i>
+                    <div class="progress progress-bar-dimensions">
+                        <div id="firstOutcomeProgressBar" class="progress-bar progress-bar-striped bg-success progress-bar-animated" 
+                        role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        ${Math.round(categoriesOutcomeRanking[0].percentageContribution * 10)/10 + '%'}</div>
+                    </div>
+                    <br>
+                    <i class="transaction-category-amount-progress-bar">${categoriesOutcomeRanking[0].amount + ' zł'}</i>
+                </div>
+                `;
+                outcomesColumn.innerHTML = firstDiv;
+                let firstProgressBar = document.getElementById("firstOutcomeProgressBar");
+                firstProgressBar.style.width = `${categoriesOutcomeRanking[0].percentageContribution}%`;
+            }
+            if(categoriesOutcomeRanking.length >= 2) {
+                secondDiv = `
+                <div>
+                    <br>
+                    <hr>
+                    <i class="transaction-category-name-progress-bar">${categoriesOutcomeRanking[1].name}</i>
+                    <div class="progress progress-bar-dimensions">
+                        <div id="secondOutcomeProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" 
+                        role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        ${Math.round(categoriesOutcomeRanking[1].percentageContribution * 10)/10 + '%'}</div>
+                    </div>
+                    <br>
+                    <i class="transaction-category-amount-progress-bar">${categoriesOutcomeRanking[1].amount + ' zł'}</i>
+                </div>
+                `;
+                outcomesColumn.innerHTML += secondDiv;
+                let secondProgressBar = document.getElementById("secondOutcomeProgressBar");
+                secondProgressBar.style.width = `${categoriesOutcomeRanking[1].percentageContribution}%`;
+            }
+            if(categoriesOutcomeRanking.length === 3) {
+                thirdDiv = `
+                <div>
+                    <br>
+                    <hr>
+                    <i class="transaction-category-name-progress-bar">${categoriesOutcomeRanking[2].name}</i>
+                    <div class="progress progress-bar-dimensions">
+                        <div id="thirdOutcomeProgressBar" class="progress-bar progress-bar-striped bg-danger progress-bar-animated"
+                             role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                             ${Math.round(categoriesOutcomeRanking[2].percentageContribution * 10)/10 + '%'}</div>
+                   </div>
+                   <br>
+                    <i class="transaction-category-amount-progress-bar">${categoriesOutcomeRanking[2].amount + ' zł'}</i>
+                </div>
+            `;
+                outcomesColumn.innerHTML += thirdDiv;
+                let thirdProgressBar = document.getElementById("thirdOutcomeProgressBar");
+                thirdProgressBar.style.width = `${categoriesOutcomeRanking[2].percentageContribution}%`;
+            }
+            console.log(data)
+        }).fail(function (data) {
+            console.log(data);
+        });
+    }
+}
